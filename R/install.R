1f8b912e5970b8e972bd06d659a8866fd28960ad
Mitchell <mitch.ohara-wild@monash.edu>
2018-12-27 11:45:24 +1100

    Updated installation documentation

--- R/install.R
+++ R/install.R
@@ -1,3 +1,16 @@
+#' Install Prophet and its dependencies
+#'
+#' @inheritParams reticulate::conda_list
+#'
+#' @param method Installation method. By default, "auto" automatically finds a
+#'   method that will work in the local environment. Change the default to force
+#'   a specific installation method.
+#'
+#' @param envname Name of Python environment to install within
+#'
+#' @param extra_packages Additional Python packages to install along with
+#'   Prophet.
+#'
 #' @export
 install_prophet <- function(method = c("auto", "virtualenv", "conda"),
                             conda = "auto",
@@ -5,10 +18,6 @@ install_prophet <- function(method = c("auto", "virtualenv", "conda"),
                             extra_packages = "pystan") {
 
   method <- match.arg(method)
-  if (identical(method, "virtualenv") && is_windows()) {
-    stop("Installing prophet into a virtualenv is not supported on Windows",
-         call. = FALSE)
-  }
 
   reticulate::py_install(c("fbprophet", extra_packages), envname = envname,
                          method = method, conda = conda)
